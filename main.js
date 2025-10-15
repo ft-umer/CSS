47f5d19b09bb7e7c5faff486ab0bde2bac686416
Syed Umer Mujahid Hassni <109479332+ft-umer@users.noreply.github.com>
2025-10-15 05:02:14 -0700

    javascript

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // let pass = 123;
 // let c_pass = 123;
 
-
 // if (pass == c_pass) {
 //   text = "Submitted";
 // } else {
@@ -10,7 +9,6 @@
 
 // let age = 20;
 // let city = "FSD";
-
 
 // let text = !(age >= 18) ? "Age must be 18" : (city !== "FSD") ? "Not Eligible" : "Eligible";
 
@@ -22,44 +20,79 @@
 
 // let text = 25 - (discount * price);
 
+// let day;
+// let date = new Date().getDay();
 
-let day;
-let date = new Date().getDay();
+// switch(date){
+//     case 1:
+//     day = "Monday";
+//     break;
 
-switch(date){
-    case 1:
-    day = "Monday";
-    break;
-    
-    case 2:
-    day = "Tuesday";
-    break;
-    
-    case 3:
-    day = "Wednesday";
-    break;
-    
-    case 4:
-    day = "Thursday";
-    break;
-    
-    case 5:
-    day = "Friday";
-    break;
-    
-    case 6:
-    day = "Saturday";
-    break;
-    
-    case 7:
-    day = "Sunday";
-}
+//     case 2:
+//     day = "Tuesday";
+//     break;
 
-let text;
+//     case 3:
+//     day = "Wednesday";
+//     break;
 
-if (day == "Friday") {
-    text = "Submitted";
-}
+//     case 4:
+//     day = "Thursday";
+//     break;
+
+//     case 5:
+//     day = "Friday";
+//     break;
+
+//     case 6:
+//     day = "Saturday";
+//     break;
+
+//     case 7:
+//     day = "Sunday";
+// }
+
+// let text;
+
+// if (day == "Friday") {
+//     text = "Submitted";
+// }
+
+// let text = "";
+// let table = 5;
+// let i = 1;
+
+// while (i <= 10) {
+//   const element = i * table;
+//   text += table + " x " + i + " = " + element + "<br>";
+//   i++;
+// }
 
 
-document.getElementById("result").innerHTML = text;
+// const person = {};
+
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+
+
+
+// document.getElementById("result").innerHTML = person.firstName + " is " + person.age + " years old.";
+
+
+
+// myFunction computes the product of a and b
+
+
+// function class1(a, b) {
+//   return a * b;   // returns the product of a and b
+// }
+
+// class1 = (a,b,c,d) => a*b + c*d;
+
+// let result = class1(4, 3);
+// document.getElementById("result").innerHTML = "The product is: " + result;
+
+
